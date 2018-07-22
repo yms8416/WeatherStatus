aae208331a6e1d0c3fccbb767664c3b20f3e7c47
Can PERK <bayms8416@gmail.com>
2018-07-22 12:35:31 +0300

    sreacrh component with workind version

--- BilgeAdam.WeatherService/BilgeAdam.WeatherService/wwwroot/js/yms.tile.js
+++ BilgeAdam.WeatherService/BilgeAdam.WeatherService/wwwroot/js/yms.tile.js
@@ -1,4 +1,5 @@
-﻿Vue.component('yms-tile', {
+﻿yms.components = {};
+yms.components.tile = Vue.component('yms-tile', {
     template: "#weatherTile",
     props: ['query'],
     data: function () {
@@ -7,9 +8,31 @@
             data = r;
         });
         return data;
+    },
+    methods: {
+        load: function (query) {
+            yms.ajax.get(query, function (r) {
+                yms.models.weather = r;
+            });
+        }
+    }
+});
+new Vue({
+    el: '#root'
+});
+yms.components.search = Vue.component("yms-search", {
+    template: '<input type="text" placeholder="Search" data-role="input" v-model="query" v-on:keyup.enter="search" />',
+    data: function () {
+        return { query : "" };
+    },
+    methods: {
+        search: function () {
+            yms.components.tile.options.methods.load(this.query);
+            this.query = "";
+        }
     }
 });
 
 new Vue({
-    el: '#root'
+    el: '#searchArea'
 });
